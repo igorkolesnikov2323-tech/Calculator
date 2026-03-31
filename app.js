9f3f3bc5af495ac9468de2dd8a96ec93c6ecd33c
Ihor Kolesnikov <igorkolesnikov2323@gmail.com>
2026-03-31 14:10:51 +0200

    added all files

---
+++ app.js
@@ -0,0 +1,13 @@
+VANTA.NET({
+    el: ".wrapper",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 0xff0053,
+    backgroundColor: 0x0,
+    spacing: 14.00
+})
---
+++ logic.js
@@ -0,0 +1,52 @@
+let a = ''
+
+const buttons = document.querySelectorAll('.grid__item')
+const input = document.querySelector('#value')
+const qual = ['=']
+const backspace = ['<']
+const clear = ['AC']
+const procent = ['%']
+const operator = ['-','+','/','*']
+const opposite = ['+/-']
+const his = document.querySelector('.history')
+input.value = '0'
+
+buttons.forEach(btn => {    
+    btn.addEventListener('click', function ddd(event) {
+        let t = event.target.textContent
+        if (!qual.includes(t) && !procent.includes(t) && !opposite.includes(t) && !backspace.includes(t)){
+            input.value=a+=t 
+        }
+        if(qual.includes(t)){ // Button =
+            input.value=(eval(a))
+            a = eval(a)
+            let hystoryElement = document.createElement('h1')
+            hystoryElement.innerText = input.value
+            his.append(hystoryElement)
+            hystoryElement.className = 'history__item'
+        }
+        if (clear.includes(t)){ //Button AC
+            input.value='0'
+            a = ''
+        }
+        if(procent.includes(t) && !operator.some(op => a.includes(op))){ //Button %
+            a += '/100'
+        }
+        if(opposite.includes(t)){ // Button +/-
+            a = -a
+            input.value = -input.value 
+        } 
+    });
+});
+
+his.addEventListener('click', function(event){
+    let t = event.target
+    let tContent = event.target.textContent
+    if (t.classList.contains('history__item')) {
+        input.value = tContent
+        a = tContent
+    }
+})
+
+//let tokens = a.match(/\d+|[+\-*/]/g)
+
